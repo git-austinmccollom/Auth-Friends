270c4bf82c1188dccabef5229c7c952ca35cbf47
git-austinmccollom <austinmccollom@gmail.com>
2020-09-15 16:48:07 -0500

    setup many of the files and router

---
+++ friends/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+
+import Friendster from "./Components/Friendster";
+import Login from "./Components/Login";
+
+function App() {
+  return (
+    <Router>
+      <header>
+        <Link to="/login">Login</Link>
+        <Link to="/friendster">Friendster</Link>
+        <Switch>
+          {/* <PrivateRoute exact path="/friendster" component={Friendster} /> */}
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </header>
+    </Router>
+  );
+}
+
+export default App;
---
+++ friends/src/Components/Friendster.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Friendster(props) {
+    
+    return(
+        <h1>Friendster</h1>
+    )
+}
