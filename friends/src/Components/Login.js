18ba6d39bfc1289c340cd094b98e62b6e33c2116
git-austinmccollom <austinmccollom@gmail.com>
2020-09-15 17:07:12 -0500

    login form

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -1,10 +1,46 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
+const initialValue = {
+    username: '',
+    password: ''
+}
 
 export default function Login(props) {
-    
-    return(
-        <h1>Login</h1>
-    )
+
+    const [ credentials, setCredentials ] = useState(initialValue);
+    const [ error, setError ] = useState('');
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials({
+            ...credentials,
+            [name]: value
+        })
+    }
+
+    const login = (e) => {
+        e.preventDefault();
+    }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
 }
